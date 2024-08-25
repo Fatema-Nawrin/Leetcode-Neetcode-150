0e2c03d570e4f14f5309711eec8819b7f13f159c
Fatema-Nawrin <nawrin4000@gmail.com>
2024-08-25 23:56:38 +0600

    productExceptSelf added

---
+++ productExceptSelf.ts
@@ -0,0 +1,18 @@
+function productExceptSelf(nums: number[]): number[] {
+  const result: Array<number> = new Array(nums.length).fill(1);
+  console.log('Int=', result);
+
+  for (let i = 1; i < nums.length; i++) {
+    result[i] = nums[i - 1] * result[i - 1];
+  }
+  let rightProduct = 1;
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    result[i] = result[i] * rightProduct;
+    rightProduct *= nums[i];
+  }
+
+  return result;
+}
+
+console.log(productExceptSelf([1, 2, 3, 4]));
