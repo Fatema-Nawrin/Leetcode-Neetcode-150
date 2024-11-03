a9b9f11d20a9ea075258c50c1f5ea3da41b96ab8
Fatema-Nawrin <nawrin4000@gmail.com>
2024-11-03 22:08:50 +0600

    twopointers updated

--- twopointers.py
+++ twopointers.py
@@ -84,7 +84,6 @@ def trap(height) -> int:
     if not height:
         return 0
     l, r = 0, len(height) - 1
-    # Track max height seen from left and right
     leftMax, rightMax = height[l], height[r]
     res = 0
     while l < r:
