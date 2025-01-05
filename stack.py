42d2b1a3f6df4221e208e0bdfeeda11ee910796a
Fatema-Nawrin <nawrin4000@gmail.com>
2025-01-06 04:42:48 +0600

    Valid Parentheses added

--- stack.py
+++ stack.py
@@ -6,6 +6,7 @@ def isValid(self, s: str) -> bool:
         if char in mapping.values():
             stack.append(char)
         elif char in mapping.keys():
+            # check last element in stack
             if stack and stack[-1] == mapping[char]:
                 stack.pop()
             else:
