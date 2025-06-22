b36d8b2b23bfc72d6d36efbd650acf70f7d4af76
Fatema-Nawrin <nawrin4000@gmail.com>
2025-06-22 22:46:23 +0100

    min stack added

--- stack.py
+++ stack.py
@@ -17,3 +17,31 @@ def isValid(self, s: str) -> bool:
 
 print(isValid("([]){}"))  # True
 print(isValid("([)]"))  # False
+
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        # Empty lists are "falsy" in Python, so not [] evaluates to True
+        # [-1] means "last element"
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+        else:
+            self.min_stack.append(self.min_stack[-1])
+
+    def pop(self) -> None:
+        if self.stack:
+            self.stack.pop()
+            self.min_stack.pop()
+
+    def top(self) -> int:
+        self.stack[-1]
+
+    def getMin(self) -> int:
+        if self.min_stack:
+            return self.min_stack[-1]
